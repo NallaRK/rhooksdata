1050ad6b89f904bee82c88c6370377c66fd3f41e
Ramakrishna Nalla <ramakrishna.nalla@gmail.com>
2019-08-21 21:17:52 -0700

    Updates

---
+++ precache-manifest.b9045090cde7f2484a3e8cbb5b9b3ceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cee60259eb0026390ccb94e576de16",
+    "url": "/rhooksdata/index.html"
+  },
+  {
+    "revision": "d8558a19462a0d3400ef",
+    "url": "/rhooksdata/static/css/main.b21f3c8d.chunk.css"
+  },
+  {
+    "revision": "535efc8643f924456171",
+    "url": "/rhooksdata/static/js/2.c50a125b.chunk.js"
+  },
+  {
+    "revision": "d8558a19462a0d3400ef",
+    "url": "/rhooksdata/static/js/main.8d5a5f4e.chunk.js"
+  },
+  {
+    "revision": "b21be562684dcba20644",
+    "url": "/rhooksdata/static/js/runtime~main.4089c881.js"
+  }
+]);
